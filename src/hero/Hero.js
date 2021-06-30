0c23ff34e613c756fe2f39dd0b29ddc583b7a15f
Samuel Fang <samuelfangjw@gmail.com>
2021-07-01 04:08:39 +0800

    Add NavBar component

--- src/hero/Hero.js
+++ src/hero/Hero.js
@@ -2,11 +2,19 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="py-20 bg-gradient-to-r from-blue-500 to-indigo-900">
-      Hero
-    </div>
+    <section>
+      <div className="py-20 bg-gradient-to-r from-blue-500 to-indigo-900">
+        <div className="container mx-auto p-5 text-white">
+          <h1 className="font-semibold text-3xl">
+            Hi there! 👋 I&apos;m Samuel,
+          </h1>
+          <p>
+            A computer science student at the National University of Singapore.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
 export default Hero;
-ƒ
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,7 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
+
+const mobileButton = (shape) => (
+  <svg
+    className="block h-6 w-6"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    aria-hidden="true"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d={shape}
+    />
+  </svg>
+);
+
+const closeButton = mobileButton("M4 6h16M4 12h16M4 18h16");
+const openButton = mobileButton("M6 18L18 6M6 6l12 12");
+
+const navItems = ["Projects", "Teaching"];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {isOpen ? openButton : closeButton}
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <img
+                className="block lg:hidden h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                alt="Workflow"
+              />
+              <img
+                className="hidden lg:block h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                alt="Workflow"
+              />
+            </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                {navItems.map((item) => (
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    key={item}
+                  >
+                    {item}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Transition
+        show={isOpen}
+        enter="transition ease-out duration-100 transform"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="transition ease-in duration-75 transform"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            {navItems.map((item) => (
+              <a
+                href="#"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                key={item}
+              >
+                {item}
+              </a>
+            ))}
+          </div>
+        </div>
+      </Transition>
+    </nav>
+  );
 };
 
 export default Navbar;
