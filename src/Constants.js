f182e756e594c0e7e9e69aaed3b258d2c69d2ee8
Samuel Fang <samuelfangjw@gmail.com>
2021-07-01 22:06:57 +0800

    Add socials to hero section

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,9 @@
 const email = "samuelfangjw@gmail.com";
+const mailto = "mailto:" + email;
 
 const links = {
   github: "https://github.com/samuelfangjw",
   linkedin: "https://www.linkedin.com/in/samuelfangjw/",
 };
 
-export { email, links };
+export { email, mailto, links };
--- src/hero/Hero.js
+++ src/hero/Hero.js
@@ -1,4 +1,26 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { mailto, links } from "../Constants";
+
+const socialButton = (icon, link) => (
+  <a href={link} className="mr-3">
+    <FontAwesomeIcon icon={icon} size="lg"/>
+  </a>
+);
+
+const email = socialButton(faEnvelope, mailto);
+const github = socialButton(faGithub, links.github);
+const linkedin = socialButton(faLinkedin, links.linkedin);
+
+const socials = (
+  <div className="mt-3">
+    {email}
+    {github}
+    {linkedin}
+  </div>
+);
 
 const Hero = () => {
   return (
@@ -11,6 +33,7 @@ const Hero = () => {
           <p>
             a computer science student at the National University of Singapore.
           </p>
+          {socials}
         </div>
       </div>
     </section>
