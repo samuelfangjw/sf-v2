b1c4293efdddcc14c71a7dd3373257366cbe31a0
Samuel Fang <samuelfangjw@gmail.com>
2021-07-01 21:41:14 +0800

    Update margins in project page

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -6,13 +6,13 @@ const Projects = () => {
   return (
     <section id="projects" className="mt-5">
       <div className="container m-auto text-center">
-        <h1 className="font-semibold text-3xl">Projects</h1>
+        <h1 className="font-semibold text-3xl mb-1">Projects</h1>
         <p>
-          This section features some of my work, including personal greenfield
+          This section features some of my more notable work, including personal greenfield
           projects as well as open-source software I regularly contribute to.
         </p>
       </div>
-      <div className="flex flex-wrap justify-center">
+      <div className="flex flex-wrap justify-center mt-2">
         {projectdescriptions.map((project, idx) => (
           <ProjectCard {...project} key={idx} />
         ))}
